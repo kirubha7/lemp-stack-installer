671904ef5ff2a29e3118fe8c141b580c088bd541
Kirubha7 <45505238+kirubha7@users.noreply.github.com>
2019-08-30 14:17:56 +0530

    update

--- lemp.sh
+++ lemp.sh
@@ -37,8 +37,6 @@ sudo swapon /swapfile
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get install python-certbot-nginx
 
-
-
 #Move nginx conf file to enable php support on ngnix
 echo -e "\nMoving Nginx configuration file...\n"
 sudo mv default /etc/nginx/sites-available
